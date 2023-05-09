53e0d5e2e52af8b2305750f9706b1612d25da5a4
KimSangHyeok <tkdgur0906@naver.com>
2023-05-10 00:32:13 +0900

    feat: BoardDto 생성

---
+++ src/main/java/com/jscode/board/dto/request/BoardRequest.java
@@ -0,0 +1,13 @@
+package com.jscode.board.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardRequest {
+
+    private String title;
+    private String content;
+}
---
+++ src/main/java/com/jscode/board/dto/response/BoardResponse.java
@@ -0,0 +1,27 @@
+package com.jscode.board.dto.response;
+
+import com.jscode.board.domain.Board;
+import com.jscode.board.dto.request.BoardRequest;
+import lombok.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class BoardResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public static Board toEntity(BoardRequest boardRequest) {
+        return Board.createBoard(boardRequest.getTitle(), boardRequest.getContent());
+    }
+
+    public static BoardResponse from(Board board) {
+        return BoardResponse.builder()
+                .title(board.getTitle())
+                .content(board.getContent())
+                .build();
+    }
+}
